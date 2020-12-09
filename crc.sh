cef3771466f1994aec9becce555e6f7b3b8da041
Qcza <qcza12@gmail.com>
2020-12-09 08:56:08 +0100

    feat: conditional memo component

--- crc.sh
+++ crc.sh
@@ -1,15 +1,61 @@
-#!/bin/sh
-mkdir "./$1" && mkdir "./$1/__tests__";
+#!/bin/bash
 
-cp ~/.crc/templates/Component.temp "./$1/$1.tsx"
-cp ~/.crc/templates/index.temp "./$1/index.ts"
-cp ~/.crc/templates/styles.temp "./$1/styles.ts"
-cp ~/.crc/templates/stories.temp "./$1/$1.stories.tsx"
-cp ~/.crc/templates/test.temp "./$1/__tests__/$1.test.tsx"
+unset COMP_NAME MEMO
 
-cd ./"$1" || exit;
-sed -i "s/\*\*name\*\*/$1/g" "$1.tsx";
-sed -i "s/\*\*name\*\*/$1/g" index.ts;
-sed -i "s/\*\*name\*\*/$1/g" styles.ts;
-sed -i "s/\*\*name\*\*/$1/g" "$1.stories.tsx";
-sed -i "s/\*\*name\*\*/$1/g" "./__tests__/$1.test.tsx";
+create_files() {
+  mkdir "./$COMP_NAME" && mkdir "./$COMP_NAME/__tests__"
+  cp ~/.crc/templates/index.temp "./$COMP_NAME/index.ts"
+  cp ~/.crc/templates/styles.temp "./$COMP_NAME/styles.ts"
+  cp ~/.crc/templates/stories.temp "./$COMP_NAME/$COMP_NAME.stories.tsx"
+  cp ~/.crc/templates/test.temp "./$COMP_NAME/__tests__/$COMP_NAME.test.tsx"
+}
+
+create_component() {
+  cp ~/.crc/templates/Component.temp "./$COMP_NAME/$COMP_NAME.tsx"
+}
+
+create_memo_component() {
+  cp ~/.crc/templates/Component.memo.temp "./$COMP_NAME/$COMP_NAME.tsx"
+}
+
+replace_names() {
+  cd ./"$COMP_NAME" || exit
+  sed -i "s/\*\*name\*\*/$COMP_NAME/g" "$COMP_NAME.tsx"
+  sed -i "s/\*\*name\*\*/$COMP_NAME/g" index.ts
+  sed -i "s/\*\*name\*\*/$COMP_NAME/g" styles.ts
+  sed -i "s/\*\*name\*\*/$COMP_NAME/g" "$COMP_NAME.stories.tsx"
+  sed -i "s/\*\*name\*\*/$COMP_NAME/g" "./__tests__/$COMP_NAME.test.tsx"
+}
+
+while getopts ":n:m" opt; do
+    case $opt in
+      n)
+        COMP_NAME=$OPTARG
+        ;;
+      m)
+        MEMO=true
+        ;;
+      \?)
+        echo "Invalid option: -$OPTARG" >&2
+        exit 1
+        ;;
+      :)
+        echo "Invalid option: $OPTARG requires an argument" 1>&2
+        exit 1
+        ;;
+    esac
+done
+
+
+if [ -n "$COMP_NAME" ]; then
+  create_files
+  if [ "$MEMO" = true ]; then
+    create_memo_component
+  else
+    create_component
+  fi
+  replace_names
+else
+  echo "Missing component name -n"
+  exit 1
+fi
